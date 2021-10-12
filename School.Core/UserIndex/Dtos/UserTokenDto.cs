d847d13c79968b431b5c0664714c9a5e9ce3dac6
小残 李 <2359765941@qq.com>
2021-10-12 10:17:00 +0800

    ？？

---
+++ School.Core/UserIndex/Dtos/UserTokenDto.cs
@@ -0,0 +1,10 @@
+
+namespace School.Core.UserIndex.Dtos
+{
+    public class UserTokenDto
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string[] Role { get; set; }
+    }
+}
